829f973fdc53a7f653f36d8d2c69f4ac0c1fa0c4
wazaraki <wazaraki@gmail.com>
2016-02-16 02:28:39 -0300

    update menu.js file

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,7 @@
 
 'use strict';
 (() => {
-  console.log('it works');
+  console.log('Nothing to see here :)');
+  // I made the menu with CSS only but found no compelling way to
+  // transform the menu layout into a mobile version
 })();
